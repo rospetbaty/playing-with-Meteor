1ba46e821a0e7cdc568c5dc80dbf128cc59862fe
rospetbaty <petros@sourced.tech>
2017-03-05 11:22:33 +0100

    First Commit

---
+++ imports/ui/body.js
@@ -0,0 +1,44 @@
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
+import './body.html';
+
+Template.body.onCreated(function bodyonCreated() {
+  this.state = new ReactiveDict();
+});
+
+Template.body.helpers({
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    //prevent default browser form submit
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+
+    Meteor.call('tasks.insert', tex)
+
+    target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
+});
